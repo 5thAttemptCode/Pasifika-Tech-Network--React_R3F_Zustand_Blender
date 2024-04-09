adbfe64fb9126020244e65f31628f1040bceef26
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-09 23:21:20 +0200

    Updated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Navbar from './components/nav'
 import About from './pages/about'
+import Footer from './components/footer'
+import Contact from './pages/contact'
+import StudyBot from './pages/studyBot'
+import ScrollToTop from './utils/scrollToTop'
 
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={ <Home />} />
         <Route path="/about" element={ <About />} />
+        <Route path="/studybot" element={ <StudyBot />} />
+        <Route path="/contact" element={ <Contact />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
