c5d0a97734edfc5b28008eb38648a792d5b95d81
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-09 23:54:36 +0200

    new data StudyBot

---
+++ src/components/data/aboutData/studyBot/index.jsx
@@ -0,0 +1,40 @@
+const StudyBotData = [
+    {
+        question: "How do I set up a pomodoro timer?",
+        title:"Setup A Pomodoro Timer",
+        textImage: [{
+                    text:"First, join one of the study rooms in the STUDY CENTRE",
+                    image: "https://pasifikatechnetwork.github.io/assets/images/study-rooms-25a1c0470543ec096ab09ce81201c8f7.png"
+                   },
+                   {
+                    text:`Once you have joined, run a !pomodoro command in the #study-lounge to set your timer.
+
+                    Use the the format !pomodoro <study><break>
+                    
+                    e.g. !pomodoro 50,10 for a 50 min study session with a 10 min break.
+                    
+                    You will see this in the #study-lounge if it is successful:`,
+                    image: "https://pasifikatechnetwork.github.io/assets/images/pomodoro-success-031bf611dc1ea7e120e0875a8d11faa6.png"
+                   }]
+    },
+
+    {
+        question: "How do I set myself Study Goals using StudyBot?",
+        title:"KEEP TRACK OF YOUR GOALS",
+        textImage: [{
+                    text:"Step one, set yourself, for example, a STUDY TIME goal of 2 hours this week",
+                    image: "https://pasifikatechnetwork.github.io/assets/images/studygoals-1-aa90b999d78075c00097cb0e8e90a0bc.png"
+                   },
+                   {
+                    text:"Step two, add some specific goals in WORDS e.g.",
+                    image: "https://pasifikatechnetwork.github.io/assets/images/studygoals-2-f99ef6097ab373e870a8ccf4291f5501.png"
+                   },
+                   {
+                    text:"When you complete your goal, MARK it done",
+                    image: "https://pasifikatechnetwork.github.io/assets/images/studygoals-3-8bdd7495c7332f66401275df51eb2ee0.png"
+                   }
+                ]
+    }
+]
+
+export default StudyBotData
