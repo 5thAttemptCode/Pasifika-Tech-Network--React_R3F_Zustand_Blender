1bb272447db5141b1abd0e67d40529f923c6b1be
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-09 23:19:30 +0200

    Added responsive Menu, improved Nav responsiveness

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './style.css'
-import { Link } from 'react-router-dom'
-import { ArrowSquareOut } from 'phosphor-react'
+import ResponsiveMenu from './components/responsiveMenu'
+import { Link } from 'react-router-dom' 
+import { DotsThreeOutline, DotsThreeOutlineVertical } from 'phosphor-react'
+
 
 export default function Navbar() {
+
+  const [ menuVisible, setMenuVisible ] = useState(false)
+  const menuButtonRef = useRef(null)
+
+  const handleLinkClick = () => {
+    setMenuVisible(false)
+  }
+
+  const handleOutsideClick = (event) => {
+    if (menuVisible && !event.target.closest('.responsive-menu') && event.target !== menuButtonRef.current) {
+      setMenuVisible(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('click', handleOutsideClick)
+
+    return () => {
+      window.removeEventListener('click', handleOutsideClick)
+    }
+  }, [menuVisible])
+
   return (
-    <nav>
-      <Link className='logo' to="/">
-        <div>
-            <img src="https://pasifikatechnetwork.github.io/img/ptn-logo-trans.png" alt="" />
-        </div>
-        Pasifika Tech Network
-      </Link>
-      <ul>
-        <Link to="/">Subscribe</Link>
-        <Link to="/about">About Us</Link>
-        <a href="https://github.com/pasifikatechnetwork/pasifikatechnetwork.github.io" target="_blank">
-          Github&nbsp; 
-          <div><ArrowSquareOut size={18} /></div>
-        </a>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <Link className='logo' to="/">
+          <div>
+            <img src="https://pasifikatechnetwork.github.io/img/ptn-logo-trans.png" />
+          </div>
+          Pasifika Tech Network
+        </Link>
+        <ul>
+          <a href="https://discord.com/" target="_blank">Discord</a>
+          <button ref={menuButtonRef} onClick={() => setMenuVisible(prev => !prev)}>
+            Menu &nbsp; 
+            {!menuVisible ? 
+              <DotsThreeOutline className='icon' size={20} /> 
+              : 
+              <DotsThreeOutlineVertical  className='iconSecondary' size={20} />
+            }
+          </button>
+        </ul>
+      </nav>
+      <ResponsiveMenu 
+        isVisible={menuVisible} 
+        handleLinkClick={handleLinkClick} 
+      />
+    </>
   )
 }
