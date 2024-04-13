4ef80f3d0244d3ddabbf13ef6dca4a3fe1a79c5f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-13 23:23:18 +0200

    added files in new folder

---
+++ src/components/nav/components/responsiveMenu/responsiveMenu/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './style.css'
+import { NavLink } from 'react-router-dom'
+
+
+export default function ResponsiveMenu({ isVisible, handleLinkClick }) {
+  
+  return (
+    <div className='responsive-menu' style={{ right: isVisible ? '4vw' : '-100%' }}>
+      <ul>
+        <NavLink onClick={handleLinkClick} to="/">Home</NavLink>
+        <NavLink onClick={handleLinkClick} to="/about">About Us</NavLink>
+        <NavLink onClick={handleLinkClick} to="/studybot">StudyBot</NavLink>
+        <NavLink onClick={handleLinkClick} to="/contact">Contact</NavLink>
+      </ul>
+      <div className="subscribe-box">
+        <p>Subscribe to our newsletter</p>
+        <input type="text"/>
+      </div>
+    </div>
+  )
+}
+
+
+
+        {/* <a onClick={handleLinkClick}  
+          href="https://github.com/pasifikatechnetwork/pasifikatechnetwork.github.io" 
+          target="_blank"
+        >
+          Github&nbsp; 
+          <div><ArrowSquareOut size={18} /></div>
+        </a> */}
