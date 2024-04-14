948f565385fc33ba715efae49512602eff93178d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-14 22:56:31 +0200

    Updated code-structure

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './style.css'
+import SubscribeForm from '../subscribeForm'
+import useItemStore from '../../store/store'
 
 
 export default function Modal({ isModalVisible }) {
 
+  const toggleModalVisible = useItemStore((state) => state.toggleModalVisible)
+
+  const handleClick = (event) => {
+    // prevent event propagation 
+    event.stopPropagation()
+  }
+
   return (
-    <div className='modal' style={{ display: isModalVisible ? 'block' : 'none' }}>
-      
+    <div onClick={toggleModalVisible} className='modal' style={{ display: isModalVisible ? 'block' : 'none' }}>
+      <div onClick={handleClick}>
+        <SubscribeForm />
+      </div>
     </div>
   )
 }
