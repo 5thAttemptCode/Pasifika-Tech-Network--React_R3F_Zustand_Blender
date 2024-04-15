4b814b3b363d00bf39f6269e680761b5327062b0
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-15 16:32:50 +0200

    Updated content + css

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import './style.css'
+import SubHeader from '../../components/subHeader'
+import SubHeaderSecondary from '../../components/subHeaderSecondary'
+import { DiscordLogo, GithubLogo, YoutubeLogo } from 'phosphor-react'
+
 
 export default function Contact() {
   return (
-    <div>
-      
-    </div>
+    <main className='contact'>
+      <SubHeader text="Get in touch" />
+      <div className="contact-wrapper">
+        <div className="mail">
+          <SubHeaderSecondary text="Send us an email" />
+          <h4>contact@pasifikateched.net</h4>
+        </div>
+        <div className="other-contact">
+          <SubHeaderSecondary text="Find us also here" />
+          <a href="https://www.youtube.com/@TechNesianLiveStream" target="_blank">Youtube &nbsp; <YoutubeLogo size={22} /></a>
+          <a href="https://discord.com/" target="_blank">Discord &nbsp; <DiscordLogo size={22} /></a>
+          <a href="https://github.com/pasifikatechnetwork/pasifikatechnetwork.github.io" target="_blank">GitHub &nbsp; <GithubLogo size={22} /></a>
+        </div>
+      </div>
+    </main>
   )
 }
