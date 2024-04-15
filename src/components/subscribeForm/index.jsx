d44660fb3cc56c8d24d729a8287b16cee1d44aa0
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-15 16:35:37 +0200

    Added prop

--- src/components/subscribeForm/index.jsx
+++ src/components/subscribeForm/index.jsx
@@ -4,10 +4,11 @@ import SubHeaderSecondary from '../subHeaderSecondary'
 import { ArrowRight } from 'phosphor-react'
 
 
-export default function SubscribeForm() {
+export default function SubscribeForm({ infoMessage = false, message = "Subscribe to our monthly newsletter!" }) {
   return (
     <div className="form-container">
       <SubHeaderSecondary text="Subscribe to our Newsletter" />
+      { infoMessage && <p>{message}</p> }
       <form>
         <input type="email" placeholder='Your Email' />
         <button><ArrowRight size={25} /></button>
